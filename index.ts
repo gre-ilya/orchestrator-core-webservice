e15affc949230b3224bdf20bd75752620903bc78
gre-ilya <ilya.grebenyuk.01@mail.ru>
2023-05-15 13:54:48 +0500

    Configuring...

--- index.ts
+++ index.ts
@@ -1,11 +1,15 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import * as swaggerUi from 'swagger-ui-express';
+// import { execa } from 'execa'
+import swaggerDocument from './swagger.json';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
+app.use('/api', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
 });
