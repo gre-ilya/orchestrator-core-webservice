aa75cf38013f5d446ed92e2220715c6e62bd8d39
gre-ilya <ilya.grebenyuk.01@mail.ru>
2023-05-15 12:46:30 +0500

    Configuring...

--- index.ts
+++ index.ts
@@ -10,6 +10,10 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
 });
 
+app.get('/deployment', (req: Request, res: Response) => {
+  res.send(`/deployment`);
+})
+
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
